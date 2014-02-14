a7ea9d94bb5c8cd86e2e049be020c12b6ce2b146
Brian Beloshapka <bbelos@hotmail.com>
2014-02-14 11:53:10 -0600

    Add smali/baksmali tools.

---
+++ epicmtd/re-smali.sh
@@ -0,0 +1,2 @@
+java -Xmx512M -jar smali-1.3.2.jar newWiMAXHiddenMenu/out/classes/ -o proprietary/WiMAXHiddenMenu.apk.parts/classes.dex
+java -Xmx512M -jar smali-1.3.2.jar newWiMAXSettings/out/classes/ -o proprietary/WiMAXSettings.apk.parts/classes.dex
